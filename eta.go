9e0e5bece8e0bcf91ba6f58d4599422cc4bbda11
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-08-02 15:49:00 +0800

    Add GetBusArrivalV2

--- eta.go
+++ eta.go
@@ -7,12 +7,15 @@ import (
 
 // ArrivingBus contains information about an incoming bus at a bus stop.
 type ArrivingBus struct {
+	OriginCode       string
+	DestinationCode  string
 	EstimatedArrival string
 	Latitude         string
 	Longitude        string
 	VisitNumber      string
 	Load             string
 	Feature          string
+	Type             string
 }
 
 // Service contains information about a bus service at a bus stop.
@@ -25,12 +28,15 @@ type Service struct {
 	NextBus        ArrivingBus
 	SubsequentBus  ArrivingBus
 	SubsequentBus3 ArrivingBus
+	NextBus2       ArrivingBus
+	NextBus3       ArrivingBus
 }
 
 // BusArrival contains information about incoming buses at a bus stop.
 type BusArrival struct {
 	OdataMetadata string `json:"odata.metadata"`
 	BusStopID     string
+	BusStopCode   string
 	Services      []Service
 }
 
@@ -40,7 +46,9 @@ type GetBusArrivalOptions struct {
 	SST       bool
 }
 
-// GetBusArrival returns real-time bus arrival information at a bus stop from the LTA DataMall API.
+// GetBusArrival returns real-time bus arrival information at a bus stop from the LTA DataMall API. Returns information
+// for a specific bus service or all services if options.serviceNo is empty. Returns timestamps in SST format if
+// options.SST is true.
 func (c APIClient) GetBusArrival(busStopID string, options *GetBusArrivalOptions) (BusArrival, error) {
 	req, err := http.NewRequest("GET", c.Endpoint+"/BusArrival", nil)
 	if err != nil {
@@ -60,6 +68,49 @@ func (c APIClient) GetBusArrival(busStopID string, options *GetBusArrivalOptions
 		if options.SST {
 			q.Add("SST", "True")
 		}
+	}
+
+	req.URL.RawQuery = q.Encode()
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return BusArrival{}, err
+	}
+	defer res.Body.Close()
+
+	var busArrival BusArrival
+	err = json.NewDecoder(res.Body).Decode(&busArrival)
+	if err != nil {
+		return BusArrival{}, err
+	}
+
+	return busArrival, nil
+}
+
+// GetBusArrivalV2 returns real-time bus arrival information from the LTA DataMall API. Returns information for a
+// specific bus service or all services if serviceNo is empty.
+//
+// Changes from GetBusArrival:
+//   - BusStopID attribute in BusArrival renamed to BusStopCode.
+//   - New Type attribute in ArrivingBus.
+//   - New OriginCode and DestinationCode attributes in ArrivingBus, replacing OriginatingID and TerminatingID in Service.
+//   - Removal of Status attribute from ArrivingBus. Services which are not in operation will not be included in the
+//   response.
+//   - SubsequentBus and SubsequentBus3 attributes renamed to NextBus2 and NextBus3.
+//   - Removal of SST parameter.
+func (c APIClient) GetBusArrivalV2(busStopCode string, serviceNo string) (BusArrival, error) {
+	req, err := http.NewRequest("GET", c.Endpoint+"/BusArrivalv2", nil)
+	if err != nil {
+		return BusArrival{}, err
+	}
+
+	req.Header.Set("AccountKey", c.AccountKey)
+
+	q := req.URL.Query()
+	q.Add("BusStopCode", busStopCode)
+
+	if serviceNo != "" {
+		q.Add("ServiceNo", serviceNo)
 	}
 
 	req.URL.RawQuery = q.Encode()
--- eta_test.go
+++ eta_test.go
@@ -1,6 +1,7 @@
 package datamall
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -349,4 +350,108 @@ func TestAPIClient_GetBusArrival(t *testing.T) {
 			t.Fail()
 		}
 	})
+}
+
+func TestAPIClient_GetBusArrivalV2(t *testing.T) {
+	t.Parallel()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		w.Write([]byte(`{"odata.metadata":"http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element","BusStopCode":"96049","Services":[{"ServiceNo":"2","Operator":"GAS","NextBus":{"OriginCode":"99009","DestinationCode":"10589","EstimatedArrival":"2017-08-02T15:38:49+08:00","Latitude":"1.3438965","Longitude":"103.96221783333333","VisitNumber":"1","Load":"SEA","Feature":"WAB","Type":"DD"},"NextBus2":{"OriginCode":"99009","DestinationCode":"10589","EstimatedArrival":"2017-08-02T15:50:58+08:00","Latitude":"1.3739711666666667","Longitude":"103.97514966666667","VisitNumber":"1","Load":"SEA","Feature":"WAB","Type":"DD"},"NextBus3":{"OriginCode":"99009","DestinationCode":"10589","EstimatedArrival":"2017-08-02T15:57:50+08:00","Latitude":"0","Longitude":"0","VisitNumber":"1","Load":"SEA","Feature":"WAB","Type":"DD"}},{"ServiceNo":"24","Operator":"SBST","NextBus":{"OriginCode":"54009","DestinationCode":"54009","EstimatedArrival":"2017-08-02T15:40:56+08:00","Latitude":"1.3445355","Longitude":"103.968438","VisitNumber":"1","Load":"SDA","Feature":"WAB","Type":"SD"},"NextBus2":{"OriginCode":"54009","DestinationCode":"54009","EstimatedArrival":"2017-08-02T15:53:59+08:00","Latitude":"1.359649","Longitude":"103.99118333333334","VisitNumber":"1","Load":"SEA","Feature":"WAB","Type":"SD"},"NextBus3":{"OriginCode":"54009","DestinationCode":"54009","EstimatedArrival":"2017-08-02T16:06:00+08:00","Latitude":"1.341015","Longitude":"103.9712825","VisitNumber":"1","Load":"SEA","Feature":"WAB","Type":"SD"}}]}`))
+	}))
+	defer ts.Close()
+
+	client := APIClient{
+		Endpoint: ts.URL,
+		Client:   http.DefaultClient,
+	}
+
+	etas, err := client.GetBusArrivalV2("", "")
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+
+	expected := BusArrival{
+		OdataMetadata: "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
+		BusStopCode:   "96049",
+		Services: []Service{
+			{
+				ServiceNo:     "2",
+				Operator:      "GAS",
+				NextBus: ArrivingBus{
+					OriginCode:       "99009",
+					DestinationCode:  "10589",
+					EstimatedArrival: "2017-08-02T15:38:49+08:00",
+					Latitude:         "1.3438965",
+					Longitude:        "103.96221783333333",
+					VisitNumber:      "1",
+					Load:             "SEA",
+					Feature:          "WAB",
+					Type:             "DD",
+				},
+				NextBus2: ArrivingBus{
+					OriginCode:       "99009",
+					DestinationCode:  "10589",
+					EstimatedArrival: "2017-08-02T15:50:58+08:00",
+					Latitude:         "1.3739711666666667",
+					Longitude:        "103.97514966666667",
+					VisitNumber:      "1",
+					Load:             "SEA",
+					Feature:          "WAB",
+					Type:             "DD",
+				},
+				NextBus3: ArrivingBus{
+					OriginCode:       "99009",
+					DestinationCode:  "10589",
+					EstimatedArrival: "2017-08-02T15:57:50+08:00",
+					Latitude:         "0",
+					Longitude:        "0",
+					VisitNumber:      "1",
+					Load:             "SEA",
+					Feature:          "WAB",
+					Type:             "DD",
+				},
+			}, {
+				ServiceNo:     "24",
+				Operator:      "SBST",
+				NextBus: ArrivingBus{
+					OriginCode:       "54009",
+					DestinationCode:  "54009",
+					EstimatedArrival: "2017-08-02T15:40:56+08:00",
+					Latitude:         "1.3445355",
+					Longitude:        "103.968438",
+					VisitNumber:      "1",
+					Load:             "SDA",
+					Feature:          "WAB",
+					Type:             "SD",
+				},
+				NextBus2: ArrivingBus{
+					OriginCode:       "54009",
+					DestinationCode:  "54009",
+					EstimatedArrival: "2017-08-02T15:53:59+08:00",
+					Latitude:         "1.359649",
+					Longitude:        "103.99118333333334",
+					VisitNumber:      "1",
+					Load:             "SEA",
+					Feature:          "WAB",
+					Type:             "SD",
+				},
+				NextBus3: ArrivingBus{
+					OriginCode:       "54009",
+					DestinationCode:  "54009",
+					EstimatedArrival: "2017-08-02T16:06:00+08:00",
+					Latitude:         "1.341015",
+					Longitude:        "103.9712825",
+					VisitNumber:      "1",
+					Load:             "SEA",
+					Feature:          "WAB",
+					Type:             "SD",
+				},
+			},
+		},
+	}
+	if !reflect.DeepEqual(expected, etas) {
+		fmt.Printf("Expected:\n%#v\nActual:\n%#v", expected, etas)
+		t.Fail()
+	}
 }
