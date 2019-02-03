fe3c35f2b02c9beeea50ed797cfa5e6948301369
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2019-02-03 12:53:59 +0800

    Bus Arrival: Handle empty ArrivingBus from API

--- arrival.go
+++ arrival.go
@@ -3,6 +3,7 @@ package datamall
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -17,6 +18,59 @@ type ArrivingBus struct {
 	Load             string
 	Feature          string
 	Type             string
+}
+
+func (b *ArrivingBus) UnmarshalJSON(data []byte) error {
+	type arrivingBus struct {
+		OriginCode       string
+		DestinationCode  string
+		EstimatedArrival string
+		Latitude         string
+		Longitude        string
+		VisitNumber      string
+		Load             string
+		Feature          string
+		Type             string
+	}
+	var ab arrivingBus
+	err := json.Unmarshal(data, &ab)
+	if err != nil {
+		return err
+	}
+	b.OriginCode = ab.OriginCode
+	b.DestinationCode = ab.DestinationCode
+	if ab.EstimatedArrival != "" {
+		t, err := time.Parse(time.RFC3339, ab.EstimatedArrival)
+		if err != nil {
+			return err
+		}
+		b.EstimatedArrival = t
+	}
+	if ab.Latitude != "" {
+		lat, err := strconv.ParseFloat(ab.Latitude, 64)
+		if err != nil {
+			return err
+		}
+		b.Latitude = lat
+	}
+	if ab.Longitude != "" {
+		lon, err := strconv.ParseFloat(ab.Longitude, 64)
+		if err != nil {
+			return err
+		}
+		b.Longitude = lon
+	}
+	if ab.VisitNumber != "" {
+		n, err := strconv.Atoi(ab.VisitNumber)
+		if err != nil {
+			return err
+		}
+		b.VisitNumber = n
+	}
+	b.Load = ab.Load
+	b.Feature = ab.Feature
+	b.Type = ab.Type
+	return nil
 }
 
 // Service contains information about a bus service at a bus stop.
--- arrival_test.go
+++ arrival_test.go
@@ -1,8 +1,6 @@
 package datamall
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 	"time"
 
@@ -18,8 +16,16 @@ func mustParseRFC3339(s string) time.Time {
 }
 
 func TestAPIClient_GetBusArrival(t *testing.T) {
-	client := APIClient{
-		Client: &MockHTTPClient{
+	type testCase struct {
+		Name          string
+		Body          string
+		StatusCode    int
+		Expected      BusArrival
+		ExpectedError error
+	}
+	testCases := []testCase{
+		{
+			Name: "when successful",
 			Body: `{
   "odata.metadata": "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
   "BusStopCode": "96049",
@@ -100,111 +106,251 @@ func TestAPIClient_GetBusArrival(t *testing.T) {
     }
   ]
 }`,
-		},
-	}
-
-	etas, err := client.GetBusArrival("", "")
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-
-	expected := BusArrival{
-		ODataMetadata: "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
-		BusStopCode:   "96049",
-		Services: []Service{
-			{
-				ServiceNo: "2",
-				Operator:  "GAS",
-				NextBus: ArrivingBus{
-					OriginCode:       "99009",
-					DestinationCode:  "10589",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T15:38:49+08:00"),
-					Latitude:         1.3438965,
-					Longitude:        103.96221783333333,
-					VisitNumber:      1,
-					Load:             "SEA",
-					Feature:          "WAB",
-					Type:             "DD",
-				},
-				NextBus2: ArrivingBus{
-					OriginCode:       "99009",
-					DestinationCode:  "10589",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T15:50:58+08:00"),
-					Latitude:         1.3739711666666667,
-					Longitude:        103.97514966666667,
-					VisitNumber:      1,
-					Load:             "SEA",
-					Feature:          "WAB",
-					Type:             "DD",
-				},
-				NextBus3: ArrivingBus{
-					OriginCode:       "99009",
-					DestinationCode:  "10589",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T15:57:50+08:00"),
-					Latitude:         0,
-					Longitude:        0,
-					VisitNumber:      1,
-					Load:             "SEA",
-					Feature:          "WAB",
-					Type:             "DD",
-				},
-			}, {
-				ServiceNo: "24",
-				Operator:  "SBST",
-				NextBus: ArrivingBus{
-					OriginCode:       "54009",
-					DestinationCode:  "54009",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T15:40:56+08:00"),
-					Latitude:         1.3445355,
-					Longitude:        103.968438,
-					VisitNumber:      1,
-					Load:             "SDA",
-					Feature:          "WAB",
-					Type:             "SD",
-				},
-				NextBus2: ArrivingBus{
-					OriginCode:       "54009",
-					DestinationCode:  "54009",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T15:53:59+08:00"),
-					Latitude:         1.359649,
-					Longitude:        103.99118333333334,
-					VisitNumber:      1,
-					Load:             "SEA",
-					Feature:          "WAB",
-					Type:             "SD",
-				},
-				NextBus3: ArrivingBus{
-					OriginCode:       "54009",
-					DestinationCode:  "54009",
-					EstimatedArrival: mustParseRFC3339("2017-08-02T16:06:00+08:00"),
-					Latitude:         1.341015,
-					Longitude:        103.9712825,
-					VisitNumber:      1,
-					Load:             "SEA",
-					Feature:          "WAB",
-					Type:             "SD",
+			StatusCode: 200,
+			Expected: BusArrival{
+				ODataMetadata: "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
+				BusStopCode:   "96049",
+				Services: []Service{
+					{
+						ServiceNo: "2",
+						Operator:  "GAS",
+						NextBus: ArrivingBus{
+							OriginCode:       "99009",
+							DestinationCode:  "10589",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:38:49+08:00"),
+							Latitude:         1.3438965,
+							Longitude:        103.96221783333333,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "DD",
+						},
+						NextBus2: ArrivingBus{
+							OriginCode:       "99009",
+							DestinationCode:  "10589",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:50:58+08:00"),
+							Latitude:         1.3739711666666667,
+							Longitude:        103.97514966666667,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "DD",
+						},
+						NextBus3: ArrivingBus{
+							OriginCode:       "99009",
+							DestinationCode:  "10589",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:57:50+08:00"),
+							Latitude:         0,
+							Longitude:        0,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "DD",
+						},
+					}, {
+						ServiceNo: "24",
+						Operator:  "SBST",
+						NextBus: ArrivingBus{
+							OriginCode:       "54009",
+							DestinationCode:  "54009",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:40:56+08:00"),
+							Latitude:         1.3445355,
+							Longitude:        103.968438,
+							VisitNumber:      1,
+							Load:             "SDA",
+							Feature:          "WAB",
+							Type:             "SD",
+						},
+						NextBus2: ArrivingBus{
+							OriginCode:       "54009",
+							DestinationCode:  "54009",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:53:59+08:00"),
+							Latitude:         1.359649,
+							Longitude:        103.99118333333334,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "SD",
+						},
+						NextBus3: ArrivingBus{
+							OriginCode:       "54009",
+							DestinationCode:  "54009",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T16:06:00+08:00"),
+							Latitude:         1.341015,
+							Longitude:        103.9712825,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "SD",
+						},
+					},
 				},
 			},
 		},
-	}
-	if !reflect.DeepEqual(expected, etas) {
-		fmt.Printf("Expected:\n%#v\nActual:\n%#v", expected, etas)
-		t.Fail()
-	}
-
-	t.Run("when datamall returns an error", func(t *testing.T) {
-		client := APIClient{
-			Client: &MockHTTPClient{
+		{
+			Name: "when some arriving buses are empty",
+			Body: `{
+  "odata.metadata": "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
+  "BusStopCode": "96049",
+  "Services": [
+    {
+      "ServiceNo": "2",
+      "Operator": "GAS",
+      "NextBus": {
+        "OriginCode": "99009",
+        "DestinationCode": "10589",
+        "EstimatedArrival": "2017-08-02T15:38:49+08:00",
+        "Latitude": "1.3438965",
+        "Longitude": "103.96221783333333",
+        "VisitNumber": "1",
+        "Load": "SEA",
+        "Feature": "WAB",
+        "Type": "DD"
+      },
+      "NextBus2": {
+        "OriginCode": "99009",
+        "DestinationCode": "10589",
+        "EstimatedArrival": "2017-08-02T15:50:58+08:00",
+        "Latitude": "1.3739711666666667",
+        "Longitude": "103.97514966666667",
+        "VisitNumber": "1",
+        "Load": "SEA",
+        "Feature": "WAB",
+        "Type": "DD"
+      },
+      "NextBus3": {
+        "OriginCode": "",
+        "DestinationCode": "",
+        "EstimatedArrival": "",
+        "Latitude": "",
+        "Longitude": "",
+        "VisitNumber": "",
+        "Load": "",
+        "Feature": "",
+        "Type": ""
+      }
+    },
+    {
+      "ServiceNo": "24",
+      "Operator": "SBST",
+      "NextBus": {
+        "OriginCode": "54009",
+        "DestinationCode": "54009",
+        "EstimatedArrival": "2017-08-02T15:40:56+08:00",
+        "Latitude": "1.3445355",
+        "Longitude": "103.968438",
+        "VisitNumber": "1",
+        "Load": "SDA",
+        "Feature": "WAB",
+        "Type": "SD"
+      },
+      "NextBus2": {
+        "OriginCode": "54009",
+        "DestinationCode": "54009",
+        "EstimatedArrival": "2017-08-02T15:53:59+08:00",
+        "Latitude": "1.359649",
+        "Longitude": "103.99118333333334",
+        "VisitNumber": "1",
+        "Load": "SEA",
+        "Feature": "WAB",
+        "Type": "SD"
+      },
+      "NextBus3": {
+        "OriginCode": "",
+        "DestinationCode": "",
+        "EstimatedArrival": "",
+        "Latitude": "",
+        "Longitude": "",
+        "VisitNumber": "",
+        "Load": "",
+        "Feature": "",
+        "Type": ""
+      }
+    }
+  ]
+}`,
+			StatusCode: 200,
+			Expected: BusArrival{
+				ODataMetadata: "http://datamall2.mytransport.sg/ltaodataservice/$metadata#BusArrivalv2/@Element",
+				BusStopCode:   "96049",
+				Services: []Service{
+					{
+						ServiceNo: "2",
+						Operator:  "GAS",
+						NextBus: ArrivingBus{
+							OriginCode:       "99009",
+							DestinationCode:  "10589",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:38:49+08:00"),
+							Latitude:         1.3438965,
+							Longitude:        103.96221783333333,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "DD",
+						},
+						NextBus2: ArrivingBus{
+							OriginCode:       "99009",
+							DestinationCode:  "10589",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:50:58+08:00"),
+							Latitude:         1.3739711666666667,
+							Longitude:        103.97514966666667,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "DD",
+						},
+						NextBus3: ArrivingBus{},
+					}, {
+						ServiceNo: "24",
+						Operator:  "SBST",
+						NextBus: ArrivingBus{
+							OriginCode:       "54009",
+							DestinationCode:  "54009",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:40:56+08:00"),
+							Latitude:         1.3445355,
+							Longitude:        103.968438,
+							VisitNumber:      1,
+							Load:             "SDA",
+							Feature:          "WAB",
+							Type:             "SD",
+						},
+						NextBus2: ArrivingBus{
+							OriginCode:       "54009",
+							DestinationCode:  "54009",
+							EstimatedArrival: mustParseRFC3339("2017-08-02T15:53:59+08:00"),
+							Latitude:         1.359649,
+							Longitude:        103.99118333333334,
+							VisitNumber:      1,
+							Load:             "SEA",
+							Feature:          "WAB",
+							Type:             "SD",
+						},
+						NextBus3: ArrivingBus{},
+					},
+				},
+			},
+		},
+		{
+			Name:       "when datamall returns an error",
+			StatusCode: 503,
+			ExpectedError: Error{
 				StatusCode: 503,
 			},
-		}
-
-		_, err := client.GetBusArrival("", "")
-		assert.Error(t, err)
-
-		expected := Error{
-			StatusCode: 503,
-		}
-		assert.Equal(t, expected, err)
-	})
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			client := APIClient{
+				Client: &MockHTTPClient{
+					Body:       tc.Body,
+					StatusCode: tc.StatusCode,
+				},
+			}
+			actual, err := client.GetBusArrival("", "")
+			if tc.ExpectedError != nil {
+				assert.Equal(t, tc.ExpectedError, err)
+			}
+			assert.Equal(t, actual, tc.Expected)
+		})
+	}
 }
